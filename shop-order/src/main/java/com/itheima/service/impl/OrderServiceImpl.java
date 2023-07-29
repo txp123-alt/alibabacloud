edff4c118c7aa0f93451ec658b93598803971a5c
TxpGit <1101642762@qq.com>
2023-07-30 00:34:55 +0800

    订单，用户，产品微服务

---
+++ shop-order/src/main/java/com/itheima/service/impl/OrderServiceImpl.java
@@ -0,0 +1,8 @@
+package com.itheima.service.impl;
+
+import com.itheima.service.OrderService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class OrderServiceImpl implements OrderService {
+}
---
+++ shop-product/src/main/java/com/itheima/service/impl/ProductServiceImpl.java
@@ -0,0 +1,8 @@
+package com.itheima.service.impl;
+
+import com.itheima.service.ProductService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductServiceImpl implements ProductService {
+}
